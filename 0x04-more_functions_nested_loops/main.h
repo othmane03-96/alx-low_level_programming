361b2df74a89d58bedce79b028944e5459b28bde
othmane03-96 <othmaneelkemmous@gmail.com>
2023-07-20 09:38:01 -0700

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
-#ifndef MAIN H 
-#define MAIN H
-int _putchar(char);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,3 +13,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 #endif
+
